6b085a89084a50fd5e8a69d265e4e04ce0196ec8
KarisyaLeGuerne <0341756@hc.ac.je>
2025-04-24 11:46:47 +0100

    Create add game basic page with title

---
+++ app/routes/add-game.tsx
@@ -0,0 +1,6 @@
+export default function AddGame() {
+  return (
+    <div className="container mx-auto py-20 px-4">
+      <h1 className="font-bold text-9xl text-center">Add <span> className="text-orange-500" >Game</span>
+  );
+}
