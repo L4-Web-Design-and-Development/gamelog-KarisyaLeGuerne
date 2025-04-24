28137af87be59b55469f24101101af141d0cf027
KarisyaLeGuerne <0341756@hc.ac.je>
2025-04-24 11:52:35 +0100

    fixed the game title log

--- app/routes/add-game.tsx
+++ app/routes/add-game.tsx
@@ -1,6 +1,7 @@
 export default function AddGame() {
   return (
     <div className="container mx-auto py-20 px-4">
-      <h1 className="font-bold text-9xl text-center">Add <span> className="text-orange-500" >Game</span>
+      <h1 className="font-bold text-9xl text-center">Add Game</h1>
+    </div>
   );
 }
