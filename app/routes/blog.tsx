4b8814eb8e3feba2bfa6d07b8b1b76ffdb0ba130
KarisyaLeGuerne <0341756@hc.ac.je>
2025-05-01 11:01:31 +0100

    add about page

---
+++ app/routes/blog.tsx
@@ -0,0 +1,7 @@
+export default function blog() {
+  return (
+    <div className="container mx-auto py-20 px-4">
+      <h1 className="font-bold text-9xl text-center">Blog</h1>
+    </div>
+  );
+}
---
+++ app/routes/games.tsx
@@ -0,0 +1,7 @@
+export default function games() {
+  return (
+    <div className="container mx-auto py-20 px-4">
+      <h1 className="font-bold text-9xl text-center">games</h1>
+    </div>
+  );
+}
