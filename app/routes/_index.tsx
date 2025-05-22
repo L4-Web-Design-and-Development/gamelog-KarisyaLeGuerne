28014512dabce65fbb3231e739d2ac251e669989
KarisyaLeGuerne <0341756@hc.ac.je>
2025-05-22 12:14:03 +0100

    added the hero.png on website

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,18 @@
-import { json, useLoaderData } from "@remix-run/react";
-import { LoaderFunction } from "@remix-run/node";
-import GameCard from "~/components/GameCard";
 import { PrismaClient } from "@prisma/client";
-import fallbackImage from "~/assets/svg/gamelog-logo.svg";
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/node";
+import GameCard from "~/components/GameCard";
+import gamelogFallback from "~/assets/svg/gamelog-logo.svg";
+import Hero from "~/components/Heroimage";
 
-// Define the loader function
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
 export async function loader() {
   const prisma = new PrismaClient();
 
@@ -29,17 +37,23 @@ export default function Index() {
   const { games } = useLoaderData<typeof loader>();
 
   return (
-    <div className="container mx-auto px-8 grid grid-cols-3 gap-8">
-      {games.map((game) => (
-        <GameCard
-          key={game.id}
-          id={game.id}
-          title={game.title}
-          releaseDate={game.releaseDate}
-          imageUrl={game.imageUrl || fallbackImage}
-          genre={game.category?.title || "Unknown"}
-        />
-      ))}
+    <div className="container px-8 mx-auto min-h-screen">
+      <Hero title={"Track Your Gaming Journey with Ease"} ctaText="Add Game" />
+      <div className="flex gap-8">
+        <Link to="/add-game">Add Game</Link>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {games.map((game) => (
+          <GameCard
+            key={game.id}
+            id={game.id}
+            title={game.title}
+            releaseDate={game.releaseDate}
+            imageUrl={game.imageUrl || gamelogFallback}
+            genre={game.category?.title || "Unknown"}
+          />
+        ))}
+      </div>
     </div>
   );
 }
